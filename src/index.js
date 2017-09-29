0cf1b7e492ff8f96364e629edb04221fd518a247
zhongzhi107 <zhong.zhi@163.com>
2017-09-29 12:29:30 +0800

    feat(bookshelf): 修改模型配置方式
    将配置项从process.env移动到config/model.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ export { default as Qails } from './qails';
 // export { default as setupRoutes } from './util/setup-routes';
 export { default as Resource } from './util/resource';
 export { default as envelope } from './util/response-envelope';
+export { default as features } from './util/features';
 export { increment, gauge, timing } from './util/watcher';
 export { default as base, knex, Model, Collection } from './util/base';
 export { snake } from './util/magic-case';
--- src/util/base.js
+++ src/util/base.js
@@ -7,6 +7,7 @@ import uuid from 'bookshelf-uuid';
 import jsonColumns from 'bookshelf-json-columns';
 import paranoia from 'bookshelf-paranoia';
 import magicCase from './magic-case';
+import features from './features';
 
 const {
   KNEX_CLIENT,
@@ -14,16 +15,7 @@ const {
   MYSQL_USER,
   MYSQL_PASSWORD,
   MYSQL_DATABASE,
-  MYSQL_PORT,
-  MODEL_VIRTUALS,
-  MODEL_VISIBILITY,
-  MODEL_PAGINATION,
-  MODEL_CASCADEDELETE,
-  MODEL_MASK,
-  MODEL_UUID,
-  MODEL_JSONCOLUMNS,
-  MODEL_MAGICCASE,
-  MODEL_SOFTDELETE
+  MYSQL_PORT
 } = process.env;
 
 const knexInstance = Knex({
@@ -39,52 +31,60 @@ const knexInstance = Knex({
 
 const base = bookshelf(knexInstance);
 
+// 让 Model 具有自动注册到中央位置的功能
+if (features.MODEL_REGISTRY) {
+  base.plugin('registry');
+}
+
 // 让 Model 具有返回虚拟字段的功能
-if (MODEL_VIRTUALS === 'true') {
+if (features.MODEL_VIRTUALS) {
   base.plugin('virtuals');
 }
 
 // 让 Model 调用 toJSON 方法时具有显示／隐藏某些字段的功能
-if (MODEL_VISIBILITY === 'true') {
+if (features.MODEL_VISIBILITY) {
   base.plugin('visibility');
 }
 
 // 让 Model 具有时间戳、数据校验和部分CURD功能
-base.plugin(modelBase.pluggable);
-
+if (features.MODEL_BASE) {
+  base.plugin(modelBase.pluggable);
+}
 // 让 Model 具有分页功能
-if (MODEL_PAGINATION === 'true') {
+if (features.MODEL_PAGINATION) {
   base.plugin('pagination');
 }
 
 // 让 Model 具有删除关联数据功能
-if (MODEL_CASCADEDELETE === 'true') {
+if (features.MODEL_CASCADEDELETE) {
   base.plugin(cascadeDelete);
 }
 
 // 让 Model 具有返回自定义字段的功能
-if (MODEL_MASK === 'true') {
+if (features.MODEL_MASK) {
   base.plugin(mask);
 }
 
 // 让 Model 具有自动生成UUID的功能
-if (MODEL_UUID === 'true') {
-  base.plugin(uuid);
+if (features.MODEL_UUID) {
+  const options = features.MODEL_UUID === true ? null : features.MODEL_UUID;
+  base.plugin(uuid, options);
 }
 
 // 让 Model 具有自动存储序列化对象的能力
-if (MODEL_JSONCOLUMNS === 'true') {
+if (features.MODEL_JSONCOLUMNS) {
   base.plugin(jsonColumns);
 }
 
 // 让 Model 具有自动转换对象 key 拼写的能力
-if (MODEL_MAGICCASE === 'true') {
+if (features.MODEL_MAGICCASE) {
   base.plugin(magicCase);
 }
 
 // 让 Model 具有软删除记录的能力
-if (MODEL_SOFTDELETE === 'true') {
-  base.plugin(paranoia, { field: 'deletedAt' });
+if (features.MODEL_SOFTDELETE) {
+  const options = features.MODEL_SOFTDELETE === true ? null : features.MODEL_SOFTDELETE;
+  base.plugin(paranoia, options);
 }
 
 // 外部可以base.knex取到knex client
