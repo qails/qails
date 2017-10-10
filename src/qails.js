2e850e8e58ef78fddf4e69ccf8011dc45cbd7710
zhongzhi107 <zhong.zhi@163.com>
2017-10-10 19:40:17 +0800

    chore: 优化qails中间件顺序

--- src/qails.js
+++ src/qails.js
@@ -13,25 +13,18 @@ import FileStreamRotator from 'file-stream-rotator';
 import mkdirp from 'mkdirp';
 import setupRoutes from './util/setup-routes';
 
-function parseMiddleWaveParams(middlewave) {
-  return Array.isArray(middlewave) ? {
-    middlewaveName: middlewave[0],
-    middlewaveOptions: middlewave[1]
-  } : {
-    middlewaveName: middlewave,
-    middlewaveOptions: null
-  };
-}
-
 const cwd = process.cwd();
 const {
   DOCUMENT_ROOT,
   LOG_ROOT,
   JSON_PRETTY,
+  PUG_ENABLE,
   PUG_PAGES_PATH,
   STATIC_ROOT,
+  CORS_ENABLE,
   CORS_ORIGIN,
-  CORS_ALLOW_METHODS
+  CORS_ALLOW_METHODS,
+  SESSION_ENABLE
 } = process.env;
 
 // 创建日志目录
@@ -40,82 +33,44 @@ mkdirp.sync(logDir);
 
 // create a rotating write stream
 const accessLogStream = FileStreamRotator.getStream({
-  date_format: 'YYYY-MM-DD',
-  filename: `${logDir}/access_%DATE%.log`,
+  date_format: 'YYYYMMDD',
+  filename: `${logDir}/access__%DATE%.log`,
   frequency: 'daily',
   verbose: false
 });
 
 export default class Qails {
-  constructor(options) {
-    const { middlewaves } = options || {
-      middlewaves: [
-        'static',
-        'cors',
-        'session',
-        'body',
-        'json',
-        'pug',
-        'routes'
-      ]
-    };
-
+  constructor() {
     this.koa = qs(new Koa());
     this.use(morgan('combined', { stream: accessLogStream }));
-    // eslint-disable-next-line
-    middlewaves.forEach((mw) => {
-      let {
-        // eslint-disable-next-line
-        middlewaveName,
-        middlewaveOptions
-      } = parseMiddleWaveParams(mw);
-
-      switch (middlewaveName) {
-        case 'static':
-          middlewaveOptions = middlewaveOptions || join(cwd, STATIC_ROOT || 'static');
-          this.use(serve(middlewaveOptions));
-          break;
-        case 'cors':
-          if (!middlewaveOptions) {
-            // 允许一个或多个指定某域名能访问
-            const corsOptions = {
-              origin: CORS_ORIGIN,
-              allowMethods: CORS_ALLOW_METHODS.split(',')
-            };
-            if (CORS_ORIGIN && CORS_ORIGIN !== '*') {
-              corsOptions.origin = (ctx) => {
-                const headerOrigin = ctx.request.header.origin;
-                const isValidate = CORS_ORIGIN.split(',').some(whitelist => new RegExp(whitelist).test(headerOrigin));
-                return isValidate ? headerOrigin : false;
-              };
-            }
-            middlewaveOptions = corsOptions;
-          }
-          this.use(cors(middlewaveOptions));
-          break;
-        case 'session':
-          middlewaveOptions = middlewaveOptions || {};
-          this.use(session(middlewaveOptions, this.koa));
-          break;
-        case 'body':
-          this.use(bodyParser(middlewaveOptions));
-          break;
-        case 'json':
-          middlewaveOptions = middlewaveOptions || { pretty: JSON_PRETTY === 'true' };
-          this.use(json(middlewaveOptions));
-          break;
-        case 'pug':
-          middlewaveOptions = middlewaveOptions || { viewPath: join(cwd, PUG_PAGES_PATH) };
-          (new Pug(middlewaveOptions)).use(this.koa);
-          break;
-        case 'routes':
-          middlewaveOptions = middlewaveOptions || join(cwd, DOCUMENT_ROOT, 'routes');
-          setupRoutes(this.koa, middlewaveOptions);
-          break;
-        default:
-          this.use(mw);
+    this.use(serve(join(cwd, STATIC_ROOT || 'static')));
+    if (CORS_ENABLE) {
+      const corsOptions = {
+        origin: CORS_ORIGIN,
+        allowMethods: CORS_ALLOW_METHODS.split(',')
+      };
+      // 允许一个或多个指定某域名能访问
+      if (CORS_ORIGIN && CORS_ORIGIN !== '*') {
+        corsOptions.origin = (ctx) => {
+          const headerOrigin = ctx.request.header.origin;
+          const isValidate = CORS_ORIGIN.split(',').some(whitelist => new RegExp(whitelist).test(headerOrigin));
+          return isValidate ? headerOrigin : false;
+        };
       }
-    });
+      this.use(cors(corsOptions));
+    }
+    if (SESSION_ENABLE === 'true') {
+      this.use(session({}, this.koa));
+    }
+    this.use(bodyParser());
+    this.use(json({ pretty: JSON_PRETTY === 'true' }));
+
+    if (PUG_ENABLE === 'true') {
+      const pug = new Pug({ viewPath: join(cwd, PUG_PAGES_PATH) });
+      pug.use(this.koa);
+    }
+
+    setupRoutes(this.koa, join(cwd, DOCUMENT_ROOT, 'routes'));
 
     this.server = null;
   }
