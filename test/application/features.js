2dccdc3740b501f29f473d5f566a556b614bf028
zhongzhi107 <zhong.zhi@163.com>
2017-10-19 19:55:22 +0800

    chore: Add some test case

---
+++ test/application/features.js
@@ -0,0 +1,24 @@
+import fs from 'fs-extra';
+import path from 'path';
+import should from 'should';
+import importFresh from 'import-fresh';
+
+describe('模型特征配置', () => {
+  it('项目中没有配置文件时，使用qails默认配置', () => {
+    const features = importFresh('../../src/util/features').default;
+    should(features).have.property('MODEL_REGISTRY', false);
+  });
+  it.skip('项目中存在配置文件且包含配置项时，应该使用配置文件中的配置', () => {
+    const root = 'test/application';
+    process.env.DOCUMENT_ROOT = 'test/application';
+    const configFolder = path.resolve(process.cwd(), root, 'config');
+    const modelConfig = path.resolve(configFolder, 'modle.js');
+    fs.ensureDirSync(configFolder);
+    fs.writeFileSync(modelConfig, 'module.exports = { MODEL_REGISTRY: true};');
+    const features = importFresh('../../src/util/features').default;
+    features.should.have.property('MODEL_REGISTRY', true);
+    features.should.have.property('MODEL_UUID', false);
+
+    fs.removeSync(configFolder);
+  });
+});
---
+++ test/application/server.js
@@ -0,0 +1,235 @@
+import { existsSync, unlinkSync, statSync, writeFileSync, mkdirSync, rmdirSync } from 'fs';
+import { resolve } from 'path';
+import request from 'supertest';
+import Router from 'koa-router';
+import should from 'should';
+import { format } from 'date-fns';
+import Qails from '../../src/qails';
+
+const { LOG_ROOT } = process.env;
+
+const router = new Router();
+router.get('/', async (ctx) => {
+  ctx.body = 'hello';
+});
+router.get('/json', async (ctx) => {
+  ctx.body = { hello: 'world' };
+});
+
+const timestamp = format(Date.now(), 'YYYYMMDD');
+const log = resolve(process.cwd(), LOG_ROOT, `access__${timestamp}.log`);
+
+describe('禁用所有中间件时', () => {
+  if (existsSync(log)) {
+    unlinkSync(log);
+  }
+  const app = new Qails({ middlewaves: [] });
+  app.use(router.routes());
+  const logFileExists = existsSync(log);
+
+  it('不应该在指定位置生成日志文件', () => {
+    should(logFileExists).be.false();
+  });
+
+  it('应该返回字符串', (done) => {
+    request(app.listen())
+      .get('/')
+      .expect('Content-Type', /text\/plain/)
+      .expect('Content-Length', '5')
+      .expect(200)
+      .expect('hello', done);
+  });
+
+  it('响应头中不包含access-control-allow-origin', (done) => {
+    request(app.listen())
+      .get('/')
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        res.headers.should.have.not.property('access-control-allow-origin');
+        return done();
+      });
+  });
+
+  it('应该返回JSON', (done) => {
+    request(app.listen())
+      .get('/json')
+      .expect('Content-Type', /json/)
+      .expect('Content-Length', '17')
+      .expect(200)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        res.body.should.have.property('hello', 'world');
+        return done();
+      });
+  });
+});
+
+describe('启动log中间件', () => {
+  if (existsSync(log)) {
+    unlinkSync(log);
+  }
+  const app = new Qails({ middlewaves: ['log'] });
+  app.use(router.routes());
+
+  it('应该在指定位置生成日志文件', (done) => {
+    request(app.listen())
+      .get('/')
+      .expect(200)
+      .end((err) => {
+        if (err) {
+          return done(err);
+        }
+        existsSync(log).should.be.true();
+        const { size } = statSync(log);
+        size.should.not.eql(0);
+        return done();
+      });
+  });
+});
+
+describe('启动static中间件', () => {
+  const app = new Qails({ middlewaves: [['static', __dirname]] });
+  app.use(router.routes());
+
+  it('应该能获取到存在的静态文件', (done) => {
+    request(app.listen())
+      .get('/init.js')
+      .expect(200, done);
+  });
+
+  it('不存在的文件应该返回404', (done) => {
+    request(app.listen())
+      .get('/404.js')
+      .expect(404, done);
+  });
+});
+
+describe('启动cors中间件', () => {
+  const app = new Qails({ middlewaves: ['cors'] });
+  app.use(router.routes());
+
+  it('响应头中应该包含access-control-allow-origin', (done) => {
+    request(app.listen())
+      .get('/')
+      .expect('access-control-allow-origin', '*')
+      .expect(200, done);
+  });
+});
+
+describe('启动session中间件', () => {
+  const app = new Qails({ middlewaves: ['session'] });
+  app.use(async (ctx) => {
+    ctx.session.views = (ctx.session.views || 0) + 1;
+    ctx.body = ctx.session.views;
+  });
+
+  it('第一次请求应该返回1', (done) => {
+    request(app.listen())
+      .get('/')
+      .expect(200)
+      .expect('Set-Cookie', /koa:sess/)
+      .expect('1', done);
+  });
+
+  it('第二次请求应该返回2', (done) => {
+    const server = app.listen();
+    request(server)
+      .post('/')
+      .expect('Set-Cookie', /koa:sess/)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        const cookie = res.headers['set-cookie'].join(';');
+        return request(server)
+          .get('/')
+          .set('cookie', cookie)
+          .expect('2', done);
+      });
+  });
+});
+
+describe('启动body中间件', () => {
+  // const app = new Qails({ middlewaves: ['body'] });
+  // app.use(router.routes());
+  //
+  // it('应该能获取到存在的静态文件', (done) => {
+  //   request(app.listen())
+  //     .get('/init.js')
+  //     .expect(200, done);
+  // });
+  //
+  // it('不存在的文件应该返回404', (done) => {
+  //   request(app.listen())
+  //     .get('/404.js')
+  //     .expect(404, done);
+  // });
+});
+
+describe('启动json中间件', () => {
+  const app = new Qails({ middlewaves: ['json'] });
+  app.use(async (ctx) => {
+    ctx.body = { foo: 'bar' };
+  });
+
+  it('应该返回格式化后的JSON', (done) => {
+    request(app.listen())
+      .get('/')
+      .expect('{\n  "foo": "bar"\n}', done);
+  });
+});
+
+describe('启动pug中间件', () => {
+  const filename = 'test.pug';
+  const template = resolve(__dirname, filename);
+  writeFileSync(template, `| ${filename}`);
+
+  const app = new Qails({ middlewaves: [['pug', { viewPath: __dirname }]] });
+  app.use(async (ctx) => {
+    ctx.render(filename);
+  });
+
+  it('应该返回解析后的HTML', (done) => {
+    request(app.listen())
+      .get('/')
+      .expect(filename, () => {
+        unlinkSync(template);
+        return done();
+      });
+  });
+});
+
+describe('启动routes中间件', () => {
+  const hello = 'hello';
+  const filename = 'index.js';
+  const routesRoot = resolve(__dirname, 'routes');
+  if (!existsSync(routesRoot)) {
+    mkdirSync(routesRoot);
+  }
+  const homeRouter = resolve(routesRoot, filename);
+  writeFileSync(homeRouter, `
+    import Router from 'koa-router';
+    const router = new Router();
+    router.get('/', async (ctx) => {
+      ctx.body = '${hello}';
+    });
+    export default router;
+  `);
+
+  const app = new Qails({ middlewaves: [['routes', routesRoot]] });
+
+  it('路由解析应该正常', (done) => {
+    request(app.listen())
+      .get('/')
+      .expect(hello, () => {
+        unlinkSync(homeRouter);
+        rmdirSync(routesRoot);
+        return done();
+      });
+  });
+});
