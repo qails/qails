d3106221def15f33e3f822a7452ad3afa5543205
zhongzhi107 <zhong.zhi@163.com>
2017-10-24 12:34:01 +0800

    test: Add coverall

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
-// eslint-disable-next-line
-// require('dotenv').config();
-
 export { default as Qails } from './qails';
-// export { default as setupRoutes } from './util/setup-routes';
 export { default as Resource } from './util/resource';
 export { default as envelope } from './util/response-envelope';
 export { default as features } from './util/features';
