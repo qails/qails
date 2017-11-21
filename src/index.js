73a96e9a7d06a557c03267a728f456624106c6c4
zhongzhi107 <zhong.zhi@163.com>
2017-11-21 14:14:35 +0800

    perf: 更改profile参数方式

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 export { default as Qails } from './qails';
-export { default as Resource } from './util/resource';
-export { increment, gauge, timing } from './util/watcher';
-export { default as base, Model } from './util/base';
+
+// middlewares
 export { default as accessLogMiddleware } from './middlewares/accessLog';
 export { default as bodyParserMiddleware } from './middlewares/bodyParser';
 export { default as corsMiddleware } from './middlewares/cors';
@@ -9,7 +8,12 @@ export { default as envelopeMiddleware } from './middlewares/envelope';
 export { default as graphqlMiddleware } from './middlewares/graphql';
 export { default as prettyJsonMiddleware } from './middlewares/prettyJson';
 export { default as serveMiddleware } from './middlewares/serve';
+
+// util
 export { default as pug } from './util/pug';
 export { default as session } from './util/session';
 export { default as requireAllRouters } from './util/requireAllRouters';
 export { fetchList, fetchItem, create, update, destroy } from './util/graphql';
+export { default as Resource } from './util/resource';
+export { increment, gauge, timing } from './util/watcher';
+export { default as bookshelf, Model } from './util/bookshelf';
