678f575048adbe4ae033e92fe36218f58135b497
zhongzhi107 <zhong.zhi@163.com>
2017-12-06 10:48:19 +0800

    chore: 用merge-graphql-schemas的fileLoader批量加载模块

--- examples/graphql/app.js
+++ examples/graphql/app.js
@@ -1,23 +1,28 @@
+import { join } from 'path';
 import {
   Qails,
   accessLogMiddleware,
   bodyParserMiddleware,
+  corsMiddleware,
   prettyJsonMiddleware,
   graphqlMiddleware
 } from 'qails';
 import { makeExecutableSchema } from 'graphql-tools';
-import { typeDefs, resolvers } from './util/mergeGraphqlSchema';
-
+import { mergeResolvers, mergeTypes, fileLoader } from 'merge-graphql-schemas';
 
 const port = 12345;
+const fullpath = pattern => join(__dirname, pattern);
+const typesArray = fileLoader(fullpath('models/**/schema.js'));
+const resolversArray = fileLoader(fullpath('models/**/resolver.js'));
 const executableSchema = makeExecutableSchema({
-  typeDefs,
-  resolvers
+  typeDefs: mergeTypes(typesArray),
+  resolvers: mergeResolvers(resolversArray)
 });
 
 const app = new Qails([
   accessLogMiddleware(),
   bodyParserMiddleware(),
+  corsMiddleware(),
   prettyJsonMiddleware(),
   graphqlMiddleware({
     schema: executableSchema
